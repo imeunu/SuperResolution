3c1eb889079c6de18e8fece50748329bced6a1a1
EunWoo Im <imeunukr@gmail.com>
2021-07-21 23:06:10 +0900

    Update functions.py

--- initiate/functions.py
+++ initiate/functions.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+def plot(img):
+    plt.axis('off')
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+def postprocess(img):
+    img[img[:] > 255] = 255
+    img[img[:] < 0] = 0
+    img = img.astype(np.uint8)
+    return img
+
 def psnr(x, y, peak=255):
     '''
     x: images
@@ -14,3 +24,13 @@ def psnr(x, y, peak=255):
     rmse = np.sqrt(np.mean(diff**2))
     result = 20 * np.log10(_max/rmse)
     return result
+
+def save_model(model, save_path):
+    model_json = model.to_json()
+    with open(save_path+"/model.json".format(acc), 'w') as json_file:
+        json_file.write(model_json)
+
+    model.save_weights(save_path +"/final_weight.h5")
+    model_json = model.to_json()
+    with open(save_path+"/model.json".format(acc), 'w') as json_file:
+        json_file.write(model_json)
