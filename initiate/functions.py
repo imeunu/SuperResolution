439b95ad9242ef71bd088e0155347abfeb99b592
EunWoo Im <imeunukr@gmail.com>
2021-07-25 00:12:09 +0900

    Update functions.py

--- initiate/functions.py
+++ initiate/functions.py
@@ -34,3 +34,10 @@ def save_model(model, save_path):
     model_json = model.to_json()
     with open(save_path+"/model.json".format(acc), 'w') as json_file:
         json_file.write(model_json)
+
+def rgb2ycbcr(img):
+    '''Convert RGB numpy array into YCbCr numpy array'''
+    r = img[:, :, 0]; g = img[:, :, 1]; b = img[:, :, 2]
+    y = 0.299 * r + 0.587 * g + 0.114 * b
+    cb = -0.169 * r - 0.331 * g + 0.5 * b
+    cr = 0.5 * r - 0.419 * g - 0.081 * b
