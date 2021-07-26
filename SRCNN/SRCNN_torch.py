efa1932f30e63e6397602e3a6aa29f03abcc4ae5
EunWoo Im <imeunukr@gmail.com>
2021-07-26 23:17:52 +0900

    Create SRCNN_torch.py

---
+++ SRCNN/SRCNN_torch.py
@@ -0,0 +1,109 @@
+import argparse
+import random
+
+import h5py
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=64,kernel_size=9)
+        self.conv2 = nn.Conv2d(in_channels=64,out_channels=32,kernel_size=1)
+        self.conv3 = nn.Conv2d(in_channels=32,out_channels=1,kernel_size=5)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
+
+class Trainset(Dataset):
+    def __init__(self, h5_file):
+        super(Trainset, self).__init__()
+        self.h5_file = h5_file
+
+    def __getitem__(self, idx):
+        with h5py.File(self.h5_file, 'r') as f:
+            lr = np.expand_dims(f['lr'][idx], axis = 0)
+            hr = np.expand_dims(f['hr'][idx], axis = 0)
+            return lr, hr
+
+    def __len__(self):
+        with h5py.File(self.h5_file, 'r') as f:
+            return len(f['lr'])
+
+def train(args):
+    train_data = Trainset(args.train_file)
+    train_dataloader = DataLoader(dataset=train_data,
+                                  batch_size=args.batch_size,
+                                  shuffle=True,
+                                  num_workers=0,
+                                  pin_memory=True,
+                                  drop_last=True)
+    model = SRCNN().to(device)
+
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(params=[
+                {"params": model.conv1.parameters()}, 
+                {"params": model.conv2.parameters()}, 
+                {"params": model.conv3.parameters(), 
+                 "lr": args.lr * 0.1}], lr=args.lr)
+
+    for epoch in range(args.epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader):
+            low, high = data
+            low = low.to(device)
+            high = high.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(low)
+            loss = criterion(outputs, high)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Training Finished')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no_cuda', action='store_true', default=True, help='Disables CUDA training.')
+    parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+    parser.add_argument('--train_path', type = str, required = True)
+    parser.add_argument('--batch_size', type=int, default=4, help='Number of epochs to train.')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train.')
+    parser.add_argument('--lr', type=float, default=0.001, help='Initial learning rate.')
+    parser.add_argument('--momentum', type=float, default=0.9, help='Weight decay (L2 loss on parameters).')
+    parser.add_argument('--save_pth', action='store_true', default=False, help='Save the checkpoint or not')
+
+    args = parser.parse_args()
+
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+
+    with h5py.File(args.train_path,'r') as f:
+        hr = np.array(f['hr'])
+        lr = np.array(f['lr'])
+    hr = torch.from_numpy(hr)
+    lr = torch.from_numpy(lr)
+
+    train(args)
+    
+# model checkpoint, learning rate reduction, evaluation
