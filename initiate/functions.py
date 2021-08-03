4726c28ddcb34fb42013abc3dc02ae0bddf89af2
EunWoo Im <imeunukr@gmail.com>
2021-08-03 15:25:02 +0900

    Update functions.py

--- initiate/functions.py
+++ initiate/functions.py
@@ -1,6 +1,15 @@
+import os
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+
+def walk(folder):
+    '''Walk through every files in a directory'''
+    for dirpath, dirs, files in os.walk(folder):
+        for filename in files:
+            yield dirpath, filename
 
 def plot(img):
     plt.axis('off')
@@ -34,16 +43,37 @@ def save_model(model, save_path):
     with open(save_path+"/model.json", 'w') as json_file:
         json_file.write(model_json)
 
+def rgb2y(img):
+    if type(img) == np.ndarray:
+        y = 16. + (64.738 * img[:,:,0] + 129.057 * img[:,:,1] + 25.064 * img[:,:,2]) / 256.
+        return np.array([y]).transpose([1,2,0])
+    elif type(img) == torch.Tensor:
+        if len(img.shape) == 4:
+            img = img.squeeze(0)
+        y = 16. + (64.738 * img[0,:,:] + 129.057 * img[1,:,:] + 25.064 * img[2,:,:]) / 256.
+    else:
+        raise TypeError('Unknown Type', type(img))
+
 def rgb2ycbcr(img):
-    '''Convert RGB numpy array into YCbCr numpy array'''
-    r = img[:, :, 0]; g = img[:, :, 1]; b = img[:, :, 2]
-    y = 0.299 * r + 0.587 * g + 0.114 * b
-    cb = -0.169 * r - 0.331 * g + 0.5 * b
-    cr = 0.5 * r - 0.419 * g - 0.081 * b
-    return np.array([y,cb,cr])
+    if type(img) == np.ndarray:
+        if len(np.shape(img)) > 3:
+            img = np.squeeze(img)
+        y = 16. + (64.738 * img[:,:,0] + 129.057 * img[:,:,1] + 25.064 * img[:,:,2]) / 256.
+        cb = 128. + (-37.945 * img[:,:,0] - 74.494 * img[:,:,1] + 112.439 * img[:,:,2]) / 256.
+        cr = 128. + (112.439 * img[:,:,0] - 94.154 * img[:,:,1] - 18.285 * img[:,:,2]) / 256.
+        return np.array([y, cb, cr]).transpose([1, 2, 0])
+    elif type(img) == torch.Tensor:
+        if len(img.shape) == 4:
+            img = img.squeeze(0)
+        y = 16. + (64.738 * img[0,:,:] + 129.057 * img[1,:,:] + 25.064 * img[2,:,:]) / 256.
+        cb = 128. + (-37.945 * img[0,:,:] - 74.494 * img[1,:,:] + 112.439 * img[2,:,:]) / 256.
+        cr = 128. + (112.439 * img[0,:,:] - 94.154 * img[1,:,:] - 18.285 * img[2,:,:]) / 256.
+        return torch.cat([y, cb, cr], 0).permute(1, 2, 0)
+    else:
+        raise TypeError('Unknown Type', type(img))
 
 def ycbcr2rgb(img):
-    y = img[:, :, 0]; cb = img[:, :, 1]; cr = img[:, :, 2]
+    y = img[:,:,0]; cb = img[:,:,1]; cr = img[:,:,2]
     r = y + 1.5748 * cr
     g = y - 0.18732 * cb - 0.46812 * cr
     b = y + 1.8556 * cb
