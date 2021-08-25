93e6bc1c20879dfffec2cd44c9aae10f5a7b6554
EunWoo Im <imeunukr@gmail.com>
2021-08-25 16:36:16 +0900

    Update vdsr_dataset.py

--- VDSR/vdsr_dataset.py
+++ VDSR/vdsr_dataset.py
@@ -4,6 +4,8 @@ import os
 import cv2
 import h5py
 
+from functions import walk
+
 def get_residual(img, scale):
     height, width, _ = img.shape
     lr = cv2.resize(img,(height//scale,width//scale), cv2.INTER_CUBIC)
@@ -14,20 +16,30 @@ def get_residual(img, scale):
 def generate(args):
     try: os.mkdir(args['save_path'])
     except: pass
-    h5 = h5py.File(args['save_path'],'w')
+    h5 = h5py.File(os.path.join(args['save_path'],'train_291.h5'),'w')
     lows, highs, scales = [], [], [2,3,4]
 
-    for path in glob.glob('*.{}'.format(args['img_ext'])):
-        hr = cv2.imread(path, cv2.IMREAD_COLOR).astype('float')
+    for folder, filename in walk(args['img_dir']):
+        ext = os.path.splitext(filename)[-1]
+        if not ext in args['img_ext']:
+            continue
+        path = os.path.join(folder,filename).replace(r'\\\\\\\\\\\\\\\','/')
+        hr = cv2.imread(path, cv2.IMREAD_COLOR)
         hr = cv2.cvtColor(hr, cv2.COLOR_BGR2YCrCb)
         y = cv2.split(hr)[0]
         for scale in scales:
             residual = get_residual(hr,scale)
-            lows.append(residual)
-            highs.append(y)
+            lows = sliceNappend(residual,lows,args)
+            highs = sliceNappend(y,highs,args)
     h5.create_dataset('residual', data = lows)
     h5.create_dataset('hr', data = highs)
     h5.close()
+
+def sliceNappend(img,imgs,args):
+    for w in range(0, img.shape[0] - args['size'] + 1, args['size']):
+        for h in range(0,img.shape[1] - args['size']+1):
+            imgs.append(img[w : w + args['size'], h : h + args['size']])
+    return imgs
 
 if __name__ == '__main__':
     args = {
