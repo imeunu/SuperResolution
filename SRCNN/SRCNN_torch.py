1248e27767306cb67a51496ac1682e19a18f0a8f
EunWoo Im <imeunukr@gmail.com>
2021-09-02 19:22:03 +0900

    Update SRCNN_torch.py

--- SRCNN/SRCNN_torch.py
+++ SRCNN/SRCNN_torch.py
@@ -9,7 +9,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 
-
 class SRCNN(nn.Module):
     def __init__(self):
         super().__init__()
