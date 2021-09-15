975884b19a11bdc9d1b744553f0ecfde330d3122
EunWoo Im <imeunukr@gmail.com>
2021-09-15 16:29:13 +0900

    Update VDSR_torch.py

--- VDSR/VDSR_torch.py
+++ VDSR/VDSR_torch.py
@@ -12,15 +12,60 @@ from torch.utils.data import DataLoader, Dataset
 class VDSR(nn.Module):
     def __init__(self):
         super().__init__()
-        # self.conv = 
+        self.input_layer = nn.Conv2d(in_channels=1,out_channels=64,
+                               kernel_size=3,stride=1,padding=1,bias=False)
+
+        residual_block = []
+        for _ in range(18):
+            residual_block.append(nn.Conv2d(in_channels=64,out_channels=64,
+                                  kernel_size=3,stride=1,padding=1,bias=False))
+            residual_block.append(nn.RelU(inplace=True))
+        self.residual_block = nn.Sequential(*residual_block)
+
+        self.output_layer = nn.Conv2d(in_channels=64,out_channels=1,
+                                      kernel_size=3,stride=1,
+                                      padding=1,bias=False)
+
+        self.weight_initialize()
+    
+    def forward(self,x):
+        low_resolution = x
+        output = self.input_layer(low_resolution)
+        output = self.residual_block(output)
+        output = self.output_layer(output)
+        return output + low_resolution
+
+    def weight_initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight,nonlinearity='relu')
+
+class Trainset(Dataset):
+    def __init__(self,h5_path):
+        super(Trainset, self).__init__()
+        self.h5_path = h5_path
+
+    def __getitem__(self, idx):
+        with h5py.File(self.h5_path, 'r') as f:
+            lr = np.array(f['lr'])
+            residual = np.array(f['residual'])
+            lr = torch.FloatTensor(lr).permute(0,3,1,2)
+            residual = torch.FloatTensor(residual).permute(0,3,1,2)
+            return lr[idx], residual[idx]
+
+    def __len__(self):
+        with h5py.File(self.h5_path, 'r') as f:
+            return len(f['lr'])
 
 def get_dataset(args):
     with h5py.File(args['data_path'],'r') as f:
-        lr = (np.array(f['lr']) - 127.5) / 255.0
+        lr = np.array(f['lr'])
         residual = np.array(f['residual'])
     return lr, residual
 
-# def 
+def train(args):
+    train_data = get_dataset(args)
+    # trainloader = 
 
 
 if __name__ == '__main__':
@@ -38,7 +83,7 @@ if __name__ == '__main__':
 
     if args['training']:
         lr, residual = get_dataset(args)
-        # train(lr, residual, args)
+        train(lr, residual, args)
 
     # if args['val_path']:
     #     evaluate(args)
